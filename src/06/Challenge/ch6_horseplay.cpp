b97b1035b51b967f47daf1eae5c547fc0d38ff48
Dimitris <dimitriosligkas@yahoo.gr>
2025-10-30 20:45:17 +0000

    Add horse play challenge

--- src/06/Challenge/ch6_horseplay.cpp
+++ src/06/Challenge/ch6_horseplay.cpp
@@ -10,6 +10,33 @@
 #include <vector>
 #include <string>
 
+// Converts a character digit to its integer value.
+int charToDigit(char c) {
+    // Throws if character is not between '0' and '9'.
+    if (c < '0' || c > '9') {
+        throw std::invalid_argument("Input character is not a digit");
+    }
+
+    /*
+        In ASCII, digit characters are sequential:
+            '0' = 48    
+            '1' = 49
+            '2' = 50
+            ...
+            '9' = 57
+
+        So subtracting '0' (48) from any digit character gives you the numeric value:
+            '0' - '0' = 0
+            '1' - '0' = 1
+            '5' - '0' = 5
+            '9' - '0' = 9
+        
+        This is a common C/C++ idiom for converting single digit characters to their integer equivalents.
+    */
+
+    return c - '0';
+}
+
 // knight_moves()
 // Summary: This function receives a string with the location of a knight in a chessboard and returns a vector of strings with the possible locations it might move to.
 // Arguments:
@@ -18,7 +45,23 @@
 std::vector<std::string> knight_moves(std::string knight){
     std::vector<std::string> moves;
 
-    // Write your code here
+    char file = knight[0];
+    int rank = charToDigit(knight[1]);
+
+    std::vector<std::pair<int, int>> offsets = {
+        {1, 2}, {2, 1}, {2, -1}, {1, -2},
+        {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}
+    };
+
+    for (auto offset : offsets) {
+        char new_file = file + offset.first;
+        int new_rank = rank + offset.second;
+
+        if (new_file >= 'a' && new_file <= 'h' && new_rank >= 1 && new_rank <= 8) {
+            std::string new_move = std::string(1, new_file) + std::to_string(new_rank);
+            moves.push_back(new_move);
+        }
+    }
 
     return moves;
 }
