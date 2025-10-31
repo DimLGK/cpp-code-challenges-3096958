0c54f13163b9d4ffd1bb9969945b10880c4804c9
Dimitris <dimitriosligkas@yahoo.gr>
2025-10-31 13:29:52 +0000

    Add relatives exercise

--- src/11/Challenge/ch11_relatives.cpp
+++ src/11/Challenge/ch11_relatives.cpp
@@ -16,10 +16,26 @@
 // Summary: This application displays the first pair of possible relatives from a list of names in a CSV file.
 int main(){
     std::fstream file ("names.csv", std::ios::in);
+    std::vector<std::string> names;
+
     // Read the CSV file.
     if(file.is_open()){
 
-        // Write your code here
+        std::string line;
+        
+        while(getline(file,line)){
+            std::stringstream  lineStream(line);
+            std::string cell;
+            while(getline(lineStream,cell,',')){
+                names.push_back(cell);
+            }
+        }
+
+        std::cout << "\nNames loaded from CSV file:\n\n" << std::flush;
+        for(size_t i=0; i<names.size(); i++){
+            std::cout << names[i] << "\n" << std::flush;
+        }
+        std::cout << "\n" << std::flush;
     
         file.close();
     }
@@ -29,6 +45,22 @@ int main(){
     }
 
     // Write your code here
+    for(size_t i=0; i<names.size(); i++){
+        std::string first_name1, last_name1;
+        std::stringstream nameStream1(names[i]);
+        nameStream1 >> first_name1 >> last_name1;
+
+        for(size_t j=i+1; j<names.size(); j++){
+            std::string first_name2, last_name2;
+            std::stringstream nameStream2(names[j]);
+            nameStream2 >> first_name2 >> last_name2;
+
+            if(last_name1 == last_name2){
+                std::cout << "Possible relatives found: " << names[i] << " and " << names[j] << "\n\n" << std::flush;    
+                return 0;
+            }
+        }
+    }
 
     std::cout << "No relatives found.\n\n" << std::flush;    
     return 0;
