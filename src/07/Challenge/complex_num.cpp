700a1a71e62a12c82bc8f00fd1330bd96c521604
Dimitris <dimitriosligkas@yahoo.gr>
2025-10-31 07:22:28 +0000

    Add complex num exercise

--- src/07/Challenge/complex_num.cpp
+++ src/07/Challenge/complex_num.cpp
@@ -1,33 +1,52 @@
 #include "complex_num.h"
 
 complex_num::complex_num(double r, double i){
-    // Write your code here
+    real = r;
+    imag = i;
 }
 
 complex_num complex_num::operator +(complex_num w){
     complex_num z;
-    // Write your code here
+    
+    z.real = real + w.real;
+    z.imag = imag + w.imag;
+
     return z;
 }
 
 complex_num complex_num::operator -(complex_num w){
     complex_num z;
-    // Write your code here
+
+    z.real = real - w.real;
+    z.imag = imag - w.imag;
+    
     return z;
 }
 
 complex_num complex_num::operator *(complex_num w){
     complex_num z;
-    // Write your code here
+    
+    z.real = (real * w.real) - (imag * w.imag);
+    z.imag = (real * w.imag) + (imag * w.real);
+
     return z;
 }
 
 complex_num complex_num::operator /(complex_num w){
     complex_num z;
-    // Write your code here
+    
+    double denominator = (w.real * w.real) + (w.imag * w.imag);
+    z.real = ((real * w.real) + (imag * w.imag)) / denominator;
+    z.imag = ((imag * w.real) - (real * w.imag)) / denominator;
+
     return z;
 }
 
 void complex_num::print(std::ostream &os){
-    // Write your code here
+    if (imag < 0)
+        os << real << " - " << imag * -1 << "i";
+    else
+        os << real << " + " << imag << "i";
+
+    os << std::flush;
 }
