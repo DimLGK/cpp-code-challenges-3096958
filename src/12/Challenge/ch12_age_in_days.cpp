1645692227662c3a96b75d5354e1433d88e6050c
Dimitris <dimitriosligkas@yahoo.gr>
2025-10-31 20:43:30 +0000

    Add age in days exercise

--- src/12/Challenge/ch12_age_in_days.cpp
+++ src/12/Challenge/ch12_age_in_days.cpp
@@ -8,6 +8,17 @@
 #include <string>
 #include <ctime>
 
+bool is_leap_year(int year){
+    if(year % 4 != 0)
+        return false;
+    else if(year % 100 != 0)
+        return true;
+    else if(year % 400 != 0)
+        return false;
+    else
+        return true;
+}
+
 // Age in Days, main()
 // Summary: This application asks the user's birth date and prints their age in days.
 int main(){
@@ -25,7 +36,46 @@ int main(){
     if(birth_m > 12)
         birth_m = 12;
 
-    // Write your code here
+    if(birth_d < 1)
+        birth_d = 1;
+    if(birth_d > 31)
+        birth_d = 31;
+
+
+    // Get today's date
+    std::time_t t = std::time(0);
+    std::tm* now = std::localtime(&t);
+    today_y = now->tm_year + 1900;
+    today_m = now->tm_mon + 1;
+    today_d = now->tm_mday;
+
+    // Calculate age in days
+    age = 0;
+    // Check full years
+    for (int year = birth_y; year < today_y; ++year){
+        if(is_leap_year(year))
+            age += 366;
+        else
+            age += 365;
+    }
+
+    // Check months in current year
+    for (int month = 1; month < today_m; ++month){
+        if(month == 2){
+            if(is_leap_year(today_y))
+                age += 29;
+            else
+                age += 28;
+        }
+        else if(month == 4 || month == 6 || month == 9 || month == 11)
+            age += 30;
+        else
+            age += 31;
+    }
+    // Add days in current month
+    age += (today_d - birth_d);
+
+    std::cout << "\nYou were born on " << birth_d << "/" << birth_m << "/" << birth_y << ".\n";
 
     if(age < 43830)
         std::cout << "You are " << age << " days old.";
