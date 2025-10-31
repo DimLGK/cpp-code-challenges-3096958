813f7b28cd8ee844703c5c9862bac9e2839403c9
Dimitris <dimitriosligkas@yahoo.gr>
2025-10-31 13:03:30 +0000

    Add tic tac toe exercise

--- src/10/Challenge/ch10_tictactoe.cpp
+++ src/10/Challenge/ch10_tictactoe.cpp
@@ -34,15 +34,143 @@ void ask_for_move(char game[][3], char mark){
 //           mark: The AI's mark: 'X' or 'O'.
 // Returns: Nothing.
 
-#define TWO_PLAYERS
+// #define TWO_PLAYERS
 void make_move(char game[][3], char mark){ 
-    #ifdef TWO_PLAYERS
-    ask_for_move(game,mark);
-    #else
-    
-    // Write your code here and comment out the definition of TWO_PLAYERS above
+    //#ifdef TWO_PLAYERS
+    //ask_for_move(game,mark);
+    //#else
 
-    #endif
+    // Step 1: Win if possible
+    int i, j;
+    char opponent = mark == 'X' ? 'O' : 'X';
+
+    // Horizontal and vertical victories
+    for(i=0; i<3; i++){ 
+        if(game[i][0]== ' ' && game[i][1]==mark && game[i][2]==mark){
+            game[i][0] = mark;
+            return;
+        }
+        if(game[i][0]==mark && game[i][1]== ' ' && game[i][2]==mark){
+            game[i][1] = mark;
+            return;
+        }
+        if(game[i][0]==mark && game[i][1]==mark && game[i][2]== ' '){
+            game[i][2] = mark;
+            return;
+        }
+        if(game[0][i]== ' ' && game[1][i]==mark && game[2][i]==mark){
+            game[0][i] = mark;
+            return;
+        }
+        if(game[0][i]==mark && game[1][i]== ' ' && game[2][i]==mark){
+            game[1][i] = mark;
+            return;
+        }
+        if(game[0][i]==mark && game[1][i]==mark && game[2][i]== ' '){
+            game[2][i] = mark;
+            return;
+        }
+    }
+    
+    // Diagonal victories
+    if(game[0][0]== ' ' && game[1][1]==mark && game[2][2]==mark){
+        game[0][0] = mark;
+        return;
+    }
+    if(game[0][0]==mark && game[1][1]== ' ' && game[2][2]==mark){
+        game[1][1] = mark;
+        return;
+    }
+    if(game[0][0]==mark && game[1][1]==mark && game[2][2]== ' '){
+        game[2][2] = mark;
+        return;
+    }
+
+    if(game[2][0]== ' ' && game[1][1]==mark && game[0][2]==mark){
+        game[2][0] = mark;
+        return;
+    }
+    if(game[2][0]==mark && game[1][1]== ' ' && game[0][2]==mark){
+        game[1][1] = mark;
+        return;
+    }
+    if(game[2][0]==mark && game[1][1]==mark && game[0][2]== ' '){
+        game[0][2] = mark;
+        return;
+    }
+
+    // Step 2: Block if possible
+
+    // Horizontal and vertical threats
+    for(i=0; i<3; i++){ 
+        if(game[i][0]==' ' && game[i][1]==opponent && game[i][2]==opponent){
+            game[i][0] = mark;
+            return;
+        }
+        if(game[i][0]==opponent && game[i][1]==' ' && game[i][2]==opponent){
+            game[i][1] = mark;
+            return;
+        }
+        if(game[i][0]==opponent && game[i][1]==opponent && game[i][2]==' '){
+            game[i][2] = mark;
+            return;
+        }
+        if(game[0][i]==' ' && game[1][i]==opponent && game[2][i]==opponent){
+            game[0][i] = mark;
+            return;
+        }
+        if(game[0][i]==opponent && game[1][i]==' ' && game[2][i]==opponent){
+            game[1][i] = mark;
+            return;
+        }
+        if(game[0][i]==opponent && game[1][i]==opponent && game[2][i]==' '){
+            game[2][i] = mark;
+            return;
+        }
+    }
+    
+    // Diagonal threats
+    if(game[0][0]==' ' && game[1][1]==opponent && game[2][2]==opponent){
+        game[0][0] = mark;
+        return;
+    }
+    if(game[0][0]==opponent && game[1][1]==' ' && game[2][2]==opponent){
+        game[1][1] = mark;
+        return;
+    }
+    if(game[0][0]==opponent && game[1][1]==opponent && game[2][2]==' '){
+        game[2][2] = mark;
+        return;
+    }
+
+    if(game[0][2]==' ' && game[1][1]==opponent && game[2][0]==opponent){
+        game[0][2] = mark;
+        return;
+    }
+    if(game[0][2]==opponent && game[1][1]==' ' && game[2][0]==opponent){
+        game[1][1] = mark;
+        return;
+    }
+    if(game[0][2]==opponent && game[1][1]==opponent && game[2][0]==' '){
+        game[2][0] = mark;
+        return;
+    }
+
+    // Step 3: Take the middle square if vacant
+    if(game[1][1]==' '){
+        game[1][1] = mark;
+        return;
+    }
+
+    // Step 4: Take any vacant square sequentially
+    for(i=0; i<3; i++)
+        for(j=0; j<3; j++)
+            if(game[i][j] == ' '){
+                 game[i][j] = mark;
+                 return;
+            }
+
+    //#endif
     return;
 }
 
@@ -57,9 +185,28 @@ void make_move(char game[][3], char mark){
 //                                  't': A tie.
 char game_state(char game[][3]){
 
-    // Write your code here
+    // Check rows and columns
+    for(int i=0;i<3;i++){
+        if(game[i][0]!=' ' && game[i][0]==game[i][1] && game[i][1]==game[i][2])
+            return game[i][0];
+        if(game[0][i]!=' ' && game[0][i]==game[1][i] && game[1][i]==game[2][i])
+            return game[0][i];
+    }
+    // Check diagonals
+    if(game[0][0]!=' ' && game[0][0]==game[1][1] && game[1][1]==game[2][2])
+        return game[0][0];
+    if(game[0][2]!=' ' && game[0][2]==game[1][1] && game[1][1]==game[2][0])
+        return game[0][2];
 
-    return 'a';
+    // Check for tie or active game
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(game[i][j]==' ') {
+                return 'a';
+            }
+        }
+    }
+    return 't';
 }
 
 // print_game()
